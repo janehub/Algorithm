2453c8ed4c05723f7b295972c3b6de2cc6d51ff7
icebeabea <sqrt1234@gmail.com>
2020-12-10 23:28:17 +0900

    크레인 인형뽑기게임
    크레인 인형뽑기게임 풀이

---
+++ programmers/level1/크레인인형뽑기게임/solution.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, vector<int> moves) {
+    int answer = 0;
+    vector<int> basket;
+    int doll;
+    
+    for(int i=0;i<moves.size();i++){
+    	
+    	for(int j=0;j<board.size();j++){
+    		doll = board.at(j).at(moves.at(i)-1);
+			if(doll!=0){
+				if(basket.size()==0)
+				{
+					basket.push_back(doll);
+				}
+				else
+				{	
+					/*비교연산*/
+					if(basket.back()==doll)
+					{
+						/*바스켓에서 제거*/
+						basket.pop_back();
+						answer+=2;
+					}
+					else
+					{	
+					/* 바스켓에 넣기 */
+					basket.push_back(doll);
+					}
+				}
+				
+				board.at(j).at(moves.at(i)-1) = 0;
+				
+				break;
+				
+			}
+   		 }
+
+    }
+    
+    return answer;
+}
