ee509068d989652bff94a52ae953e0e2ec07f64d
janehub <sqrt1234@gmail.com>
2020-12-14 11:44:03 +0900

    Update solution.cpp

--- programmers/level1/완주하지 못한 선수/solution.cpp
+++ programmers/level1/완주하지 못한 선수/solution.cpp
@@ -1,1 +1,27 @@
+#include <string>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    int i=0;
+    
+    /* 오름차순 정렬 */
+    sort(participant.begin(),participant.end());
+    sort(completion.begin(),completion.end());
+    
+    for(i=0;i<completion.size();i++)
+    {
+        if(participant[i]!=completion[i])
+        {
+            answer = participant[i];
+            break;
+        }
+    }
+    
+    answer = participant[i];
+        
+    return answer;
+}
