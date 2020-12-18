6058feffa870743a2befb8aabc552e6de4b95495
janehub <sqrt1234@gmail.com>
2020-12-18 23:00:56 +0900

    Create solution.cpp

---
+++ programmers/level1/가운데 글자 가져오기/solution.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    
+    int len = s.length();
+    
+    if(len%2==1)
+    {
+        answer=s.substr(len/2,1);
+    }
+    else
+    {
+        answer=s.substr(len/2-1,2);
+    }
+    
+    return answer;
+}
