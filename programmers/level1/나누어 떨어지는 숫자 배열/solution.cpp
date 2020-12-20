5cdf9b36b40cd86d8b7b75cad726e4d44e0cd9b3
janehub <sqrt1234@gmail.com>
2020-12-20 13:52:27 +0900

    Create solution.cpp

---
+++ programmers/level1/나누어 떨어지는 숫자 배열/solution.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    
+    for(int a:arr)
+    {
+        if(a%divisor==0)
+            answer.push_back(a);
+    }
+    
+    sort(answer.begin(),answer.end());
+    
+    if(answer.empty())
+        answer.push_back(-1);
+    
+    return answer;
+}
