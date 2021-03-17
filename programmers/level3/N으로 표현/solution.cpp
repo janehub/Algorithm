216dd2152bc9e38ebe465c5dc9bc2d43db684969
janehub <sqrt1234@gmail.com>
2021-03-17 22:26:05 +0900

    Update solution.cpp
    괄호()가 들어갈 경우 추가, 테스트케이스 2번 통과못함

--- programmers/level3/N으로 표현/solution.cpp
+++ programmers/level3/N으로 표현/solution.cpp
@@ -1,6 +1,7 @@
 import java.util.HashSet;
 import java.util.Vector;
 import java.util.Iterator;
+import java.util.ArrayList;
 
 class Solution {
     
@@ -17,39 +18,52 @@ class Solution {
     
     public int solution(int N, int number) {
         int answer = 0;
+        HashSet<Integer>[] list = new HashSet[32000+2];
         HashSet<Integer> set = new HashSet<>();
-        
         set.add(N);
+        list[1] = set;
+        
+        if(number==N)
+            return 1;
+
         for(int i=2;i<=8;i++)
         {
-            Iterator iter = set.iterator();
-            Vector<Integer> v = new Vector<>();
-            int add=0;int sub=0;int mul=0;int div=0;
-            
-            while(iter.hasNext())
+            HashSet<Integer> set2 = new HashSet<>();
+            for(int j=1;j<i;j++)
             {
-                int a = (int)iter.next();
-                add = a+N;
-                sub = a-N;
-                mul = a*N;
-                div = a/N;
-                if(add==number||sub==number||mul==number||div==number)
+                if(list[j]!=null)
                 {
-                    return i;
+                    Iterator iter = list[j].iterator();
+                    while(iter.hasNext())
+                    {
+                        int a = (int)iter.next();
+                        if(list[i-j]!=null)
+                        {
+                            Iterator iter2 = list[i-j].iterator();
+                            while(iter2.hasNext())
+                            {
+                                int b =(int)iter2.next();
+                                int add = a+b;
+                                int sub = a-b;
+                                int mul = a*b;
+                                int div = a/b;
+                                if(add==number||sub==number||mul==number||div==number)
+                                {
+                                    return i;
+                                }
+                                if(add!=0) set2.add(add);
+                                if(sub!=0) set2.add(sub);
+                                if(mul!=0) set2.add(mul);
+                                if(div!=0) set2.add(div);
+                            }
+                        }
+                    }
                 }
-                v.add(add);
-                v.add(sub);
-                v.add(mul);
-                v.add(div);
+                    
             }
-            for(int k=0;k<v.size();k++)
-            {
-               //사칙연산
-                set.add(v.get(k));
-            }
-            
-            //자릿수증가
-            set.add(getN(N,i));
+            set2.add(getN(N,i));
+            list[i] = set2;
+            System.out.println();
         }
         
         return -1;
